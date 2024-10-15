0b3ac24673e243f238e0a646605da529a57beaf6
adicarmel <adi.carmel@tomorrow.io>
2024-10-15 20:50:24 +0300

    feat(): add translations to song page

--- app/album/[albumId]/[songId]/page.tsx
+++ app/album/[albumId]/[songId]/page.tsx
@@ -1,11 +1,12 @@
 'use client';
-import { useParams } from 'next/navigation';
+import {useParams} from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import albumData from '../../../data/bob-dylan-discography.json';
+import translations from '../../../data/translations.heb.json';
 
 export default function SongPage() {
-    const { albumId, songId } = useParams();
+    const {albumId, songId} = useParams();
 
     if (!songId) {
         return <div>song not found</div>;
@@ -19,12 +20,36 @@ export default function SongPage() {
         return <div>song not found</div>;
     }
 
+    const songTranslation = translations.songs.find((s) => s.songId.toString() == songId);
+
     return (
         <div className="container mx-auto px-4 py-8">
             <Link href={`/album/${albumId}`} className="inline-block mb-4 text-blue-500 hover:text-blue-700">
                 &larr; Back to album
             </Link>
-            <h1 className="text-4xl font-bold mb-4">{song.name}</h1>
+            <h1 className="text-4xl font-bold mb-4">{songTranslation?.songName ? `${songTranslation.songName} / ${song.name}` : song.name}</h1>
+            {songTranslation?.songLyrics && (
+                <div dir="rtl">
+                    {songTranslation?.songLyrics.verses.map((verse, index) => {
+                        const key = `${songId}-${verse.length}-${index}`;
+                        return (
+                            <ul key={key} className="pb-6">
+                                {verse.map((line) => {
+                                    const key = `${songId}-${line}`;
+                                    return <li key={key}>{line}<br /></li>
+                                })}
+                                {index < songTranslation.songLyrics.verses.length - 1 && (
+                                    <ul className="pt-6">
+                                        {songTranslation.songLyrics.chorus.map((line, chorusIndex) => (
+                                            <li key={chorusIndex}>{line}</li>
+                                        ))}
+                                    </ul>
+                                )}
+                            </ul>
+                        )
+                    })}
+                </div>
+            )}
         </div>
     );
 }
